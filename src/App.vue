4f6172f7772861aa157a94ec2ae2ac71a26f1a12
Cyrille Guiot-Guillin <cyrille.guiot-guillin@etu-webschoolfactory.fr>
2018-05-24 21:55:45 +0200

    v1
    First version

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <router-link v-if="auth" to="/" v-on:click.native="logout()" replace>Logout</router-link>
+    <router-view @auth="setAuth" />
+  </div>
+</template>
+
+<script>
+import { data } from './db.js'
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      auth: false,
+      data
+    }
+  },
+
+  mounted () {
+    if (!this.auth) {
+      this.$router.replace({name: 'login'})
+    }
+  },
+
+  methods: {
+    setAuth (status) {
+      this.auth = status
+    },
+    logout () {
+      this.auth = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="form-container">
+    <h1>{{ msg }}</h1>
+    <form action="">
+      <input type="text" name="username" v-model="input.username" id="username" placeholder="Username">
+      <input type="password" name="password" v-model="input.password" id="password" placeholder="Password">
+      <input type="submit" value="Login" v-on:click="login()">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Bac à sable',
+      input: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+
+  methods: {
+    login () {
+      if (this.input.username !== '' && this.input.password !== '') {
+        for (var i = 0; i < this.$parent.data.length; i++) {
+          if (this.input.username === this.$parent.data[i].username && this.input.password === this.$parent.data[i].password) {
+            this.$emit('auth', true)
+            this.$router.replace({name: 'welcome'})
+            break
+          } else {
+            console.log('The username and / or the password is incorrect')
+          }
+        }
+      } else {
+        console.log('Please, enter a username and a password')
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  form {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 20vh;
+  }
+
+  input {
+    display: block;
+    max-width: 10%;
+  }
+</style>
